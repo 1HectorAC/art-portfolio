66018c875fd750986c95483dcb7b5ff14071077d
Hector Acosta <1Hector.AC@gmail.com>
2023-01-23 14:42:20 -0800

    Small edits and cleanup of code.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,20 +4,21 @@ const Home = (props) => {
     function changeFilter(f) {
         SetFilter(f);
     }
-    const imageElements = props.data.map((val) =>
-        <div className={val.tall ? "col-sm-3" : "col-sm-6"} style={{display:filter==val.format || filter=="All"?"block":"none"}}>
+    // Make art image elements. Includes width screen adjustments.
+    const imageElements = props.data.map((val, i) =>
+        <div className={val.tall ? "col-sm-3" : "col-sm-6"} style={{ display: filter === val.format || filter === "All" ? "block" : "none" }} key={"art" + i}>
             <img className="displayArt" src={require('../images/art/' + val.file)} alt=""></img>
         </div>
     );
+    // Make filter buttons.
     const filters = ["All", "Digital", "Graphite/Charcoal", "Color Pencil", "Oil Pastel"];
-    const filterElements = filters.map((val) =>
-        <button className="btn button2" onClick={() => changeFilter(val)} disabled={filter === val ? "True" : ""}>
+    const filterElements = filters.map((val, i) =>
+        <button className="btn button2" onClick={() => changeFilter(val)} disabled={filter === val ? "True" : ""} key={"filter" + i}>
             {val}
         </button>
     );
     return (
         <div className="container">
-
             <div className="row">
                 <div className="col-sm-9 offset-sm-3">
                     <div className="headerSection text-center">
@@ -30,7 +31,6 @@ const Home = (props) => {
                     <div className="row">
                         {imageElements}
                     </div>
-
                     <br />
                 </div>
             </div>
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,6 +1,5 @@
 import { Outlet, NavLink } from "react-router-dom";
 
-
 const Layout = () => {
     return (
         <>
@@ -9,7 +8,6 @@ const Layout = () => {
                 <div className="sidePanel w-25 h-100">
                     <div className="sidePanelContent text-center">
                         <div className="mx-auto">
-                            
                             <img className="profileImage" src={require("../images/userImageSmall.jpg")}   alt=""></img>
                         </div>
                         <h3><i>Hector Acosta</i></h3>
@@ -33,41 +31,35 @@ const Layout = () => {
             <div className="visibledevice text-center mobileSidePanel">
                 <br />
                 <div className="mx-auto">
-                    <p>Add Profile Image</p>
-
+                <img className="profileImage" src={require("../images/userImageSmall.jpg")}   alt=""></img>
                 </div>
                 <h3><i>Hector Acosta</i></h3>
                 <hr />
-
                 <p><i>Website by: Hector Acosta</i></p>
             </div>
             <div className="visibledevice sticky-top container">
                 <div className="smallNavbar">
                     <div className="row text-center">
                         <div className="col-4">
-                        <NavLink className={({ isActive }) => isActive ? "btn activeButton1v2" : "btn inactiveButton1v2"} to="/" exact>
+                        <NavLink className={({ isActive }) => isActive ? "btn activeButton1v2" : "btn inactiveButton1v2"} to="/" exact="true">
                             Gallery
                         </NavLink>
                         </div>
                         <div className="col-4">
-                        <NavLink className={({ isActive }) => isActive ? "btn activeButton1v2" : "btn inactiveButton1v2"} to="/About" exact>
+                        <NavLink className={({ isActive }) => isActive ? "btn activeButton1v2" : "btn inactiveButton1v2"} to="/About" exact="true">
                             About
                         </NavLink>
                         </div>
                         <div className="col-4">
-                        <NavLink className={({ isActive }) => isActive ? "btn activeButton1v2" : "btn inactiveButton1v2"} to="/Contact" exact>
+                        <NavLink className={({ isActive }) => isActive ? "btn activeButton1v2" : "btn inactiveButton1v2"} to="/Contact" exact="true">
                             Contact
                         </NavLink>
                         </div>
                     </div>
                 </div>
             </div>
-            
-            
-
             <Outlet />
         </>
-
     )
 };
 export default Layout;

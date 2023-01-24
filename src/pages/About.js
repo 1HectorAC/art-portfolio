64d66dbe4888aa2253ad4a8b2b2b466846eb600e
Hector Acosta <1Hector.AC@gmail.com>
2023-01-24 14:28:41 -0800

    Added content to About and Contact pages.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,28 @@
 const About = () => {
     return (
-        <h1>About Page</h1>
+        <div className="container text-center">
+            <div className="row">
+                <div className="col-sm-9 offset-sm-3">
+                    <div className="headerSection">
+                        <h1>About</h1>
+                    </div>
+                    <div className="container">
+                        <br />
+                        <h3>Introduction:</h3>
+                        <h5><i>This website is for displaying some of the art that I have worked on over the years.</i>
+                        </h5>
+                        <hr />
+                        <h3>Goals:</h3>
+                        <h5><i>When it comes to art, I strive to not only have fun but also improve my overall skills in
+                            creating something that looks nice.</i></h5>
+                        <hr />
+                        <h3>Experience:</h3>
+                        <h5><i>I am mostly self taught. I have no professional experience.</i></h5>
+                    </div>
+                    <br />
+                </div>
+            </div>
+        </div>
     )
 };
 export default About;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,20 @@
 const Contact = () => {
     return (
-        <h1>Contact Page</h1>
+        <div className="container text-center">
+            <div className="row">
+                <div className="col-sm-9 offset-sm-3">
+                    <div className="headerSection">
+                        <h1>Contact</h1>
+                    </div>
+                    <div className="container">
+                        <br />
+                        <h5>No Contacts for now.</h5>
+                        <h5>This website is mostly for personal use.</h5>
+                    </div>
+                    <br />
+                </div>
+            </div>
+        </div>
     )
 };
 export default Contact;
